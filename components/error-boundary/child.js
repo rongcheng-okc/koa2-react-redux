5c8267e2f7f641f2fc428156d0b6a867651252e9
wangdongdong <wangddrusell@dingtalk.com>
2019-05-09 14:34:31 +0800

    错误边界使用

--- components/error-boundary/child.js
+++ components/error-boundary/child.js
@@ -2,17 +2,47 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class child extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      obj3: {
+        name: 'RC_Okc'
+      }
+    };
+  }
+  componentWillReceiveProps() {
+    try {
+      throw(33); 
+    } catch (e) {
+      console.warn('组件内函数运行出错', e);
+    }
+    setTimeout(() => {
+      try {
+        throw(22);
+      } catch (e) {
+        console.warn('组件内函数 setTimeout 内运行出错', e);
+      }
+    }, 2000);
+    throw(11); // 这个异常将被错误边界捕获
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        obj3: null
+      });
+    }, 3000);
+  }
   render() {
+    const { obj3, } = this.state;
     return (
       <div>
         <h1>will happen wrong in this component</h1>
+        <p>此处 obj3.name，将会把 obj3 变为 null， {obj3.name}</p>
       </div>
     );
   }
 }
 
-child.propTypes = {
-
-};
+child.propTypes = {};
 
 export default child;
--- components/error-boundary/index.js
+++ components/error-boundary/index.js
@@ -4,12 +4,46 @@ import Child from './child';
 import ErrorBoundary from './boundary';
 
 class index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 1,
+      obj1: { text: '戎城1' },
+      obj2: { text: '戎城2' },
+    };
+  }
+  handleClick1 = () => {
+    this.setState({
+      title: new Date().getTime(),
+    });
+    // let a;
+    // console.log(a.name);  // 这种错不会导致应用奔溃
+  }
+
+  handleClick2 = () => {
+    this.setState({
+      obj1: null,
+    });
+  }
+
+  handleClick3 = () => {
+    this.setState({
+      obj2: null,
+    });
+  }
+
   render() {
+    const { title, obj1, obj2, } = this.state;
     return (
       <div>
         <ErrorBoundary>
-          <Child></Child>
+          <button onClick={this.handleClick1}>Button</button>
+          <button onClick={this.handleClick2}>Obj1 => null</button>
+          <button onClick={this.handleClick3}>Obj2 => null</button>
+          <Child title={title} />
+          <p>此处 obj1.name，将会把 obj1 变为 null， {obj1.name}</p>
         </ErrorBoundary>
+        <p>此处 obj2.name，将会把 obj2 变为 null， {obj2.name} 注意：这个不在错误边界的包裹内</p>
       </div>
     );
   }
